cf5864e0b7855c259315c41edab2435e83397c17
Andrii <2andrew.demchenko@gmail.com>
2022-11-15 13:24:00 +0200

    5th commit 4.5section(tabs)

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,35 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick(
-        {
-            speed: 1000,
-            prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
-            nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false,
-                        dots: true,
+// $(document).ready(function(){
+//     $('.carousel__inner').slick(
+//         {
+//             speed: 1000,
+//             prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
+//             nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
+//             responsive: [
+//                 {
+//                     breakpoint: 768,
+//                     settings: {
+//                         arrows: false,
+//                         dots: true,
                     
-                    }
-                },
-            ]
-        }
-    );
-  });
+//                     }
+//                 },
+//             ]
+//         }
+//     );
+//   });
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
