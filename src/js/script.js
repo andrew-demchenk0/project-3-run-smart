acd81519fb243fe8537452e58bcb0ae2317e6465
Andrii <2andrew.demchenko@gmail.com>
2022-11-15 21:06:51 +0200

    6th commit 5 full adaptive sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,16 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick(
-//         {
-//             speed: 1000,
-//             prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
-//             nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
-//             responsive: [
-//                 {
-//                     breakpoint: 768,
-//                     settings: {
-//                         arrows: false,
-//                         dots: true,
-                    
-//                     }
-//                 },
-//             ]
-//         }
-//     );
-//   });
-
-
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+    responsive:{
+        992:{
+            touch: true,
+
+        }
+    }
 });
 document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
@@ -33,3 +18,46 @@ document.querySelector('.prev').addEventListener('click', function () {
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
+
+$(document).ready(function(){
+    // $('.carousel__inner').slick(
+    //     {
+    //         speed: 1000,
+    //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
+    //         nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
+    //         responsive: [
+    //             {
+    //                 breakpoint: 768,
+    //                 settings: {
+    //                     arrows: false,
+    //                     dots: true,
+                    
+    //                 }
+    //             },
+    //         ]
+    //     }
+    // );
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+	function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+});
+
+
